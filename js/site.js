12d2427d3f0f578f8a3f1df0106698d723c95f0b
Helior Colorado <me@helior.info>
2015-08-23 16:00:19 -0500

    Set date of race

--- js/site.js
+++ js/site.js
@@ -53,9 +53,10 @@
 
     // Set coundown.
     $(".countdown").countdown({
-      date: "31 july 2015 23:30:00", // add the countdown's end date (i.e. 3 november 2012 12:00:00)
+      date: "19 september 2015 23:30:00", // add the countdown's end date (i.e. 3 november 2012 12:00:00)
       format: "on" // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
-    });
+    }, function() {console.log('finished')});
+
   });
 
   $(window).load(function() {
