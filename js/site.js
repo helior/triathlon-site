9afbad3308651f3f5c981ec01b775f06a5b6e00f
Helior Colorado <me@helior.info>
2015-08-30 19:06:43 -0700

    More metrics!

--- js/site.js
+++ js/site.js
@@ -1,12 +1,20 @@
 // Metrics
 var Metrics = {
-  totalRaised: 250,
+  totalRaised: 1250,
   donationGoal: 800,
   survivalCount: 13820,
-  totalInflicted: 15780
+  totalInflicted: 15780,
+  deathRatebyDisease: 58,
 };
 // Metrics (dynamic)
 Metrics.donationTotal = Math.floor(Metrics.totalRaised * 100 / Metrics.donationGoal);
+Metrics.survivalRate = Math.ceil(Metrics.survivalCount / Metrics.totalInflicted * 100);
+
+// Quotes
+var quotes = [
+  "That is Salvation. To give of love inside.<br>To keep love of life, no matter what, and to give to others. Generously.<br>— Sylvia Plath",
+  "There is another way."
+];
 
 // Source: http://stackoverflow.com/a/2901298
 function numberWithCommas(x) {
@@ -14,15 +22,10 @@ function numberWithCommas(x) {
 }
 
 (function($) {
-
-  var quotes = [
-    "That is Salvation. To give of love inside.<br>To keep love of life, no matter what, and to give to others. Generously.<br>— Sylvia Plath",
-    "There is another way."
-  ];
-
   var min_w = 300;
   var vid_w_orig;
   var vid_h_orig;
+
   function heroresize() {
     var ww = $(window).width();
     var bh = $(document).height();
@@ -36,17 +39,23 @@ function numberWithCommas(x) {
     if (scale * vid_w_orig < min_w) {
       scale = min_w / vid_w_orig;
     }
-    ;
-    $('.video #video-bg video').width(scale * vid_w_orig);
-    $('.video #video-bg video').height(scale * vid_h_orig);
-    var video_width = $('.video #video-bg video').width();
-    var video_offset = ($('.video #video-bg video').width() - $(window).width()) / 2;
-    $('.video #video-bg video').css({'left': '-' + video_offset + 'px'});
-    $('.no-video #video-bg img').width(scale * vid_w_orig);
-    $('.no-video #video-bg img').height(scale * vid_h_orig);
-    var video_width = $('.no-video #video-bg img').width();
-    var video_offset = ($('.no-video #video-bg img').width() - $(window).width()) / 2;
-    $('.no-video #video-bg img').css({'left': '-' + video_offset + 'px'});
+
+    var $video = $('.video #video-bg video');
+    $video.width(scale * vid_w_orig);
+    $video.height(scale * vid_h_orig);
+    var video_width = $video.width();
+    var video_offset = ($video.width() - $(window).width()) / 2;
+
+    var $poster = $('.no-video #video-bg img');
+    if ($poster.length) {
+
+    }
+    $video.css({'left': '-' + video_offset + 'px'});
+    $poster.width(scale * vid_w_orig);
+    $poster.height(scale * vid_h_orig);
+    var video_width = $poster.width();
+    var video_offset = ($poster.width() - $(window).width()) / 2;
+    $poster.css({'left': '-' + video_offset + 'px'});
   }
 
   $(document).ready(function(){
@@ -72,10 +81,19 @@ function numberWithCommas(x) {
       format: "on" // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
     }, function() {console.log('finished')});
 
-    $('.metric-total-raised').data('percent', Metrics.donationTotal);
+    window.scrollFireCallback = function() {
+      window.setTimeout(function() {$('.metric-treatment-count').data('easyPieChart').update(100);}, 100);
+      window.setTimeout(function() {$('.metric-death-rate').data('easyPieChart').update(Metrics.deathRatebyDisease);}, 400);
+      window.setTimeout(function() {$('.metric-survival-odds').data('easyPieChart').update(12.5);}, 700);
+      window.setTimeout(function() {$('.metric-total-raised').data('easyPieChart').update(Metrics.donationTotal);}, 1000);
+    }
+    Materialize.scrollFire([
+      {selector: '.metrics-strip', offset:100, callback: 'scrollFireCallback()'}
+    ]);
+
     $('.metric-value-donation-percent').text(Metrics.donationTotal + '%');
     $('.metric-value-donation-goal').text('$' + Metrics.donationGoal);
-    $('.metric-survival-rate').data('percent', Math.ceil(Metrics.survivalCount / Metrics.totalInflicted * 100));
+
     $('.circle-graph').easyPieChart({
       scaleColor: false,
       lineWidth: 4,
@@ -83,21 +101,28 @@ function numberWithCommas(x) {
       barColor: '#a378aa',
       trackColor: '#e7b8ef' ,
       size: 150,
-      animate: 1500,
-      onStep: function(values) {
+      // animate: 1500,
+      animate: {duration: 1000, enabled: true},
+      onStep: function(from, to, currentValue) {
         // Calculate current percent of animation completion.
-        var calcPercent = Math.ceil(values[0] * 100 / this.percentage);
-
-        // Metric: Total Raised
-        if (this.el.classList.contains('metric-total-raised')) {
-          // Animate totalRaised.
-          $('b', this.el).text(numberWithCommas(Math.ceil(Metrics.totalRaised * calcPercent / 100)));
-        }
+        var calcPercent = Math.ceil(currentValue * 100 / to);
 
         // Metric: Survival Rate
-        else if (this.el.classList.contains('metric-survival-rate')) {
-          // Animate survivalRate
-          $('b', this.el).text(Math.ceil(values[0]));
+        if (this.el.classList.contains('metric-treatment-count')) {
+          // Animate treatment count
+          $('b span', this.el).text(Math.ceil(calcPercent * 40 / 100));
+        }
+
+        // Metric: Death Rate
+        else if (this.el.classList.contains('metric-death-rate')) {
+          // Animate deathRatebyDisease
+          $('b span', this.el).text(Math.ceil(currentValue));
+        }
+
+        // Metric: Total Raised
+        else if (this.el.classList.contains('metric-total-raised')) {
+          // Animate totalRaised.
+          $('b span', this.el).text(numberWithCommas(Math.ceil(Metrics.totalRaised * calcPercent / 100)));
         }
       }
     });
