0bdbd91d15100a37326301a7c43a30f317e79ffa
Helior Colorado <me@helior.info>
2015-09-18 13:15:17 -0700

    Update race time

--- js/site.js
+++ js/site.js
@@ -89,7 +89,7 @@ function numberWithCommas(x) {
 
     // Set coundown.
     $(".countdown").countdown({
-      date: "19 september 2015 23:30:00", // add the countdown's end date (i.e. 3 november 2012 12:00:00)
+      date: "20 september 2015 6:00:00", // add the countdown's end date (i.e. 3 november 2012 12:00:00)
       format: "on" // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
     }, function() {console.log('finished')});
 
