146251adfee3bc2490bbaabb64eb85edaae7961d
Helior Colorado <me@helior.info>
2015-09-21 12:03:14 -0700

    Remove countdown now that it's over

--- js/site.js
+++ js/site.js
@@ -91,7 +91,9 @@ function numberWithCommas(x) {
     $(".countdown").countdown({
       date: "20 september 2015 6:00:00", // add the countdown's end date (i.e. 3 november 2012 12:00:00)
       format: "on" // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
-    }, function() {console.log('finished')});
+    }, function() {
+      $('.countdown').remove();
+    });
 
     window.scrollFireCallback = function() {
       window.setTimeout(function() {$('.metric-treatment-count').data('easyPieChart').update(100);}, 100);
