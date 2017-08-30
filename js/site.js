fbfeac5d7d337755ff4859998aa0c9b3bd452e3c
Helior Colorado <me@helior.info>
2017-08-30 10:56:06 -0700

    Update site.js

--- js/site.js
+++ js/site.js
@@ -88,7 +88,7 @@ function numberWithCommas(x) {
 
     // Set coundown.
     $(".countdown").countdown({
-      date: "25 august 2017 6:00:00", // add the countdown's end date (i.e. 3 november 2012 12:00:00)
+      date: "17 september 2017 7:00:00", // add the countdown's end date (i.e. 3 november 2012 12:00:00)
       format: "on" // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
     }, function() {
       // $('.countdown').remove();
